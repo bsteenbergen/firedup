3f7740102770477dc0223c137b552999161ed1dd
Brittany S <57652157+bsteenbergen@users.noreply.github.com>
2021-12-02 09:06:57 -0800

    map and markers finished

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import FireMap from "./FireMap.js"
 
 export default function App() {
   const [eventData, setEventData] = useState([])
-  // const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const fetchEvents = async () => {
-      // setLoading(true)
       const res = await fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events")
       const { events } = await res.json()
 
       setEventData(events)
-      // setLoading(false)
     }
     fetchEvents()
   }, [])
